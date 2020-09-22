a693913e755c0cd61dc30dd23f25a11f0838b4b7
Ducvu1996 <johnducvu1996@gmail.com>
2020-09-22 08:00:34 +0700

    update22/9

--- Module3/case_study/case_study_DB.sql
+++ Module3/case_study/case_study_DB.sql
@@ -137,13 +137,13 @@ insert into service(name_service ,area,number_of_floor,maximum_people,status_ser
 values('villa',100,2,7,'availabel','1000',1,1),
 		('house',100,1,4,'availabel','500',2,1),
 		('room',30,1,2,'availabel','200',2,2);
--- yêu cầu 2 
+-- task 2 
  select * from staff where (name_staff like 'H%' or name_staff like 'T%' or name_staff like 'K%') and length(name_staff)<=15;
- -- yêu cầu 3 
+ -- task 3 
 select* from customers
 where datediff(now(),birth_day)/365 >=18
 	and (address='Danang' or address='Quang Tri');
--- yêu cầu 4
+-- task 4
 select customers.id, customers.name_customer,contract_sign_date,contract_end_date,name_type_customer, count(contract.id_customer) as numberOfOrder
 from customers
 left join contract on customers.id = contract.id_customer
@@ -151,9 +151,10 @@ left join type_customer on type_customer.id = customers.id_type_customer
 where name_type_customer = 'Diamond'
 group by name_customer
 order by  numberOfOrder;
--- yêu cầu 5 
+-- task 5 
+create view statistics as
 select customers.id, customers.name_customer,name_type_customer,service.name_service,
-contract_sign_date,contract_end_date ,sum(service.price_rent+accompanied_service.price*accompanied_service.unit) as total_pay
+contract_sign_date,contract_end_date ,sum(service.price_rent+accompanied_service.price*accompanied_service.unit) as total_pay, contract_detail.amount
 from customers
 left join contract on customers.id = contract.id_customer
 left join type_customer on type_customer.id = customers.id_type_customer
@@ -162,7 +163,7 @@ left join contract_detail on contract.id= contract_detail.id_contract
 left join accompanied_service on contract_detail.id_accompanied_service= accompanied_service.id
 group by name_customer;
 
--- yêu cầu 6 
+-- task 6 
 select id_service,name_type_service,area,maximum_people,price_rent,type_service.name_type_service,contract.id_customer,contract_sign_date
 from service
 left join type_service on type_service.id = service.id_type_service
@@ -172,7 +173,7 @@ where id_service not in (
     left join contract on service.id = contract.id_service
     where datediff(contract_sign_date, '2019-01-01') >0
 );
--- yêu cầu 7
+-- task 7
 select id_service,name_type_service,area,maximum_people,price_rent,type_service.name_type_service,contract.id_customer,contract_sign_date
 from service
 left join type_service on type_service.id = service.id_type_service
@@ -184,7 +185,7 @@ and id_service not in (
     where contract_sign_date > '2018-12-31' and contract_sign_date < '2020-01-01' 
 )
 group by id_service;
--- yêu cầu 8
+-- task 8
 -- cách 1
 select distinct name_customer
 from  customers;
@@ -204,12 +205,12 @@ from customers;
  where year(contract_sign_date) = 2019
  group by  month(contract_sign_date)
  order by month ;
- -- yêu cầu 10
+ -- task 10
  select contract.id, contract_sign_date, contract_end_date, deposits, count(id_accompanied_service)
 from contract 
 left join contract_detail on contract.id = contract_detail.id_contract
 group by contract.id ;
--- yêu cầu 11
+-- task 11
  select name_accompanied_service, name_customer,type_customer.name_type_customer, address
 from accompanied_service 
 left join contract_detail on accompanied_service.id = contract_detail.id_contract
@@ -217,7 +218,110 @@ left join contract on contract.id = contract_detail.id_contract
 left join customers on customers.id = contract.id_customer
 left join type_customer on type_customer.id = customers.id_type_customer
 where type_customer.name_type_customer ='Diamond' and ( address = 'Vinh' or address = 'Quang Ngai');
--- yêu cầu 12
+-- task 12
 
+select c.id,c.contract_sign_date, s.name_staff, cs.name_customer, cs.phone, sv.name_service,c.deposits, count(cd.id) as 'amount'
+from contract c
+	left join contract_detail cd on c.id = cd.id_contract
+    left join staff s on s.id = c.id_staff
+    left join customers cs on cs.id = c.id_customer
+    left join service sv on sv.id =c.id_service
+where c.contract_sign_date > '2019-09-30' and c.contract_sign_date < '2020-01-01'
+and sv.name_service not in (
+	select sv.name_service
+    from service sv
+		left join contract c on c.id_service =sv.id
+	where c.contract_sign_date > '2018-12-31' and c.contract_sign_date < '2019-07-01' and sv.name_service is not null
+)
+group by c.id;
+-- task 13
+create view information as
+select asv.id, asv.name_accompanied_service, asv.price, asv.unit, asv.status, count(cd.id_accompanied_service) as 'amount'
+from accompanied_service as asv
+	left join contract_detail cd on asv.id= cd.id_accompanied_service
+group by asv.id;
+
+select *
+from information
+where amount in (
+	select max(amount)
+    from information
+    where amount is not null
+);
+-- task 14
+select c.id, ts.name_type_service, asv.name_accompanied_service, count(cd.id_accompanied_service) as 'so_lan_su_dung'
+from contract c
+	join contract_detail cd on c.id =cd.id_contract
+    join accompanied_service asv on cd.id_accompanied_service = asv.id
+    join service s on c.id_service = s.id
+    join type_service ts on s.id_type_service = ts.id
+group by asv.id
+having count(cd.id_accompanied_service) = 1;
+-- task 15
+select s.id, s.name_staff, lv.name_level, dp.name_department, s.phone, s.address, count(c.id_staff) as amount
+from staff s
+	left join contract c on c.id_staff= s.id
+    left join level lv on lv.id=s.id_level
+    left join department dp on dp.id= s.id_department
+where s.id not in (
+	select s.id
+    from staff s
+		left join contract c on s.id=c.id_staff
+    where year(c.contract_sign_date) = 2018 and c.id_staff is not null
+    group by c.id_staff
+    having count(c.id_staff) > 3
+)
+group by s.id
+order by s.id;
+
+-- Task 16.
+delete from staff
+where staff.id not in (
+	select list.id
+    from (
+		select s.id
+        from staff s
+			left join contract c on c.id_staff = s.id
+		where year(c.contract_sign_date) in ('2017', '2018', '2019')
+        group by s.id
+    ) as list
+);
+-- Task 17.	
+update customers
+set customers.id_type_customer = 1
+where customers.id in (
+	select st.id
+    from statistics st
+    where st.name_type_customer = 'Platinium' and st.total_pay> (10000000 / 25000)
+);
+-- Task 18.
+
+
+delete from customers
+where id in (
+	select temp.id
+	from (
+		select cs.id
+		from customers cs
+			inner join contract c on c.id_customer= cs.id
+		where year(c.contract_sign_date) < '2016'
+	) as temp
+);
+-- Task 19.	
+update accompanied_service
+set price = price * 2
+where id in (
+	select id
+    from statistics st
+    group by st.id
+    having sum(st.amount) > 10
+);
+
+-- Task 20.	
+select s.id as id, s.name_staff, s.email, s.phone, s.birth_day, s.address
+from staff s
+union all
+select cs.id, cs.name_customer, cs.email, cs.phone, cs.birth_day, cs.address
+from customers cs;
 
 
